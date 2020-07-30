19b438c905d21c977157494fbffa206b99636998
Darwin Morocho <darwin@iMac-de-Darwin.local>
2020-07-29 19:16:08 -0500

    vicinity null fixed

--- lib/blocs/pages/home/home_bloc.dart
+++ lib/blocs/pages/home/home_bloc.dart
@@ -73,9 +73,10 @@ class HomeBloc extends Bloc<HomeEvents, HomeState> {
   }
 
   goToPlace(Place place) async {
+    await Future.delayed(Duration(milliseconds: 300));
     add(GoToPlace(place));
     final CameraUpdate cameraUpdate = CameraUpdate.newLatLng(place.position);
-    await (await _mapController).moveCamera(cameraUpdate);
+    await (await _mapController).animateCamera(cameraUpdate);
   }
 
   void setMapController(GoogleMapController controller) {
